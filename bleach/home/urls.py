fb3194416cfa863aee1323650267ed77bc20e962
Agnivesh Manojkumar <agniveshtm2724@gmail.com>
2025-10-30 11:42:33 +0530

    caching configuration and requirements.txt updation

--- bleach/home/urls.py
+++ bleach/home/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('',views.home_page,name="home_page"),
     path('create/',views.create,name='create'),
-    path('edit/<pk>',views.edit,name='edit'),
-    path('delete/<pk>',views.delete,name='delete'),
+    path('edit/<int:pk>',views.edit,name='edit'),
+    path('delete/<int:pk>',views.delete,name='delete'),
     path('list/',views.list,name='list'),
 ]
